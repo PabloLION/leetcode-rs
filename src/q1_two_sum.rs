ee8b9079eda91752a9fc3e8656a0811959e16485
PabloLION <36828324+PabloLION@users.noreply.github.com>
2022-08-02 05:23:11 +0200

    feat: add another solution

--- src/q1_two_sum.rs
+++ src/q1_two_sum.rs
@@ -6,17 +6,40 @@
 
 // @lc code=start
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    #[allow(dead_code)]
+    fn two_sum_gc(nums: Vec<i32>, target: i32) -> Vec<i32> {
         use std::collections::HashMap;
         let mut map = HashMap::new();
         for (i, num) in nums.iter().enumerate() {
-            let complement = target - num;
+            let complement = target - *num;
             if let Some(j) = map.get(&complement) {
                 return vec![*j as i32, i as i32];
             }
             map.insert(num, i);
+
+            // another equivalent way to write the lines above:
+            // match map.get(&(target - *num)) {
+            //     Some(&i2) => return vec![i as i32, i2],
+            //     None => map.insert(*num, i as i32),
+            // };
         }
         vec![]
+    }
+
+    #[allow(dead_code)]
+    fn two_sum_naive(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        vec![]
+    }
+
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        Solution::two_sum_gc(nums, target)
     }
 }
 // @lc code=end
