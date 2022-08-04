5f7f4672658884dbeb1e0dd2d9d35f8ecf950bb6
PabloLION <36828324+PabloLION@users.noreply.github.com>
2022-08-04 14:40:16 +0200

    ref: use reference

--- src/helper/binary_search.rs
+++ src/helper/binary_search.rs
@@ -1,6 +1,8 @@
 //! A binary search implementation.
 //! returns the last index of the element which meet the condition.
-pub fn binary_search(vec: Vec<i32>, condition: &(dyn Fn(i32) -> bool + 'static)) -> Option<usize> {
+//! Should type better like this
+//! fn binary_search(vec: Vec<dyn Sized>, condition: dyn Fn(dyn Sized) -> (bool)) {}
+pub fn binary_search(vec: &Vec<i32>, condition: &(dyn Fn(i32) -> bool)) -> Option<usize> {
     let mut left = 0;
     let mut right = vec.len() - 1;
     while left <= right {
@@ -22,6 +24,5 @@ pub fn binary_search(vec: Vec<i32>, condition: &(dyn Fn(i32) -> bool + 'static))
 fn test_binary_search() {
     let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let condition = |x: i32| x > 5;
-    assert_eq!(binary_search(vec, &condition), Some(5));
+    assert_eq!(binary_search(&vec, &condition), Some(5));
 }
-// fn binary_search(vec: Vec<dyn Sized>, condition: dyn Fn(dyn Sized) -> (bool)) {}
