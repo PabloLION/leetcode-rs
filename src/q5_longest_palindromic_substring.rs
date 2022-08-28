59e2ae7667caf1792cac0fa4cb301700e4836699
PabloLION <36828324+PabloLION@users.noreply.github.com>
2022-08-28 23:46:30 +0200

    feat: q5

--- src/q5_longest_palindromic_substring.rs
+++ src/q5_longest_palindromic_substring.rs
@@ -7,6 +7,24 @@
 // @lc code=start
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
+        Solution::longest_palindrome_forum(s)
+    }
+    pub fn longest_palindrome_forum(s: String) -> String {
+        let mut window_size = s.len();
+        while window_size > 0 {
+            match s.as_bytes().windows(window_size).find(|slice| {
+                let iter = slice.iter();
+                iter.clone().eq(iter.clone().rev()) // cloning iterators is cheap
+            }) {
+                Some(slice) => return String::from_utf8(slice.to_vec()).unwrap_or("".to_string()),
+                None => window_size -= 1, // if no slice window of this size is a palindrome, try the next smallest window size
+            }
+        }
+        "".to_string() // if no slice is found or if len was zero, return empty string
+    }
+
+    #[allow(dead_code)]
+    pub fn longest_palindrome_github(s: String) -> String {
         let mut longest_palindrome = String::new();
         let s_chars = s.chars().collect::<Vec<char>>();
         for center in 0..s_chars.len() {
@@ -32,7 +50,6 @@ impl Solution {
                 longest_palindrome = s[k..=j].to_string();
             }
         }
-        println!("{}", longest_palindrome);
         longest_palindrome
     }
 }
